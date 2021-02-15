cc6cb56be250e4f021a3254c644df2f4c730c01b
Gregory Walton <scgwa@leeds.ac.uk>
2021-02-15 09:29:10 +0000

    Repaired the variable input for the mesh type

--- src/petsc/fem.c
+++ src/petsc/fem.c
@@ -13,44 +13,53 @@ char* meshselect(int,char **);
 void runtetgen();
 void deformmesh(int,double);
 void writetetgen(char *);
-void meshqual(void);
+void writevol();
+void meshquality(double *,double *);
+void checkqualtetgen(void);
 
-void pinit(int *,char ***,PetscMPIInt *);
-void ptsolve(bool,PetscMPIInt);
-void pfin();
+void pinit(PETSC_STRUC *,int *,char ***,PetscMPIInt *);
+void ptsolve(PETSC_STRUC *,bool);
+void pfin(PETSC_STRUC *);
+
+PETSC_STRUC petsol;
 
 int main (int argc, char **args)
 {
 	int i;
 	int imax = 16;
+	//int imax = 90;
 	PetscMPIInt psize;
 	soltype = 1; // just some boolean which change the problem type
 	//readmesh(meshselect(nargin,argsin),"../../data/3d/");
 	readmesh(meshselect(argc,args),"../../data/3d/region/innersmall/");
+	//readmesh("mesh_4","../../data/3d/region/innersmall/");
 
 	d3n4(); // generates the matrices in spare form
-	pinit(&argc,&args,&psize);
-	ptsolve(false,psize); // Solve sparse matrix using PETSc, working in serial
+	pinit(&petsol,&argc,&args,&psize);
+	ptsolve(&petsol,false); // Solve sparse matrix using PETSc, working in serial
 
 	vtkoutput(spstiff.sol,0);
-	meshqual();
 	//resoutput(spstiff.sol);
 
 	runtetgen();
 	for (i=1;i<=imax;i++)
 	{
 		printf("\nSTEP %d\n",i);
-		//deformmesh(1,.5); // rotation
-		deformmesh(2,2.); // bugle deformation
-		meshqual();
-		//writetetgen("./testoutput/testoutput");
+		//deformmesh(0,0.1); // shift deform
+		//deformmesh(1,1.); // rotation
+		deformmesh(2,5.); // bugle deformation
+		
+		meshquality(NULL,NULL);
 		if ((i%4)==0) {
 			runtetgen(); // Running TetGen from code
-			meshqual();
+			meshquality(NULL,NULL);
 		}
+		
+		//checkqualtetgen();
+		writevol();
 		vtkoutput(spstiff.sol,i);
 	}
-	pfin();
+	pfin(&petsol);
 
 	//meshvolumescale(); // Calculting the volume requirement about the distance to the interior surface
         //meshlengthscale(); // GRUMMP refinement
@@ -64,12 +73,12 @@ int main (int argc, char **args)
 char* meshselect(int argc, char** argv)
 {
 	int i;
-	char tstr[10];
+	char tstr[4];
 
 	for (i=0;i<argc;i++)
 	{
 		strncpy(tstr,argv[i],4);
-		if (strcmp(tstr,"mesh")==0)
+		if (strcmp(tstr,"mesh")==1)
 		{
 			return argv[i];
 		}
